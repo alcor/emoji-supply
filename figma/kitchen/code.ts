b85fb71390cf9109547526fe50b95bc5537a55c7
Nicholas Jitkoff <nicholas@jitkoff.com>
2023-08-07 21:14:29 -0600

    Updated plugin

--- figma/kitchen/code.ts
+++ figma/kitchen/code.ts
@@ -1,30 +1,59 @@
+let url = "https://emoji.supply/kitchen"; 
+// url = "http://localhost:8888/kitchen";
+
 figma.showUI(
-  `<script>window.location = 'https://emoji.supply/kitchen'</script>`,
+  `<script>window.location='${url}'</script>`,
   { themeColors: true, width: 360, height: 640 }
 );
 
+figma.ui.onmessage = (message) => {
+  console.log("got this from the UI", message)
+  if (message.clickedImage) {
+    dropIcon(message.clickedImage);
+  }
+}
+
+let supress = false;
 figma.on('drop', (event: DropEvent) => {
+  if (supress) return;  // Avoid duplicate drops, due to a browser bug
+
   const { items } = event;
-  if (items.length > 0 && items[0].type === 'text/uri-list') {
-    const url = items[0].data;
+  let fromBrowser = event.dropMetadata?.fromBrowser;
+  console.log("Plugin got drop", event);
 
-    let size = 64;
-    size = Math.round(size / figma.viewport.zoom);
-    
-    const node = figma.createRectangle() 
-    node.resize(size, size)
-  
-    node.x = Math.round(event.absoluteX - size / 2);
-    node.y = Math.round(event.absoluteY - size / 2);
+  let url = items.filter(item => item.type === 'text/uri-list')?.pop()?.data;
 
-    node.fills = [];
-    figma.createImageAsync(url).then(async (image: Image) => {
-      figma.currentPage.selection = [node];
-      node.fills = [{
-        type: 'IMAGE',
-        imageHash: image.hash,
-        scaleMode: 'FILL'
-      }]
-    })
+  if (url) {
+     if (fromBrowser) {
+      let position = figma.viewport.center; //figma.activeUsers[0]?.position || 
+      console.log("drop position", position);
+      dropIcon(url, position.x, position.y);
+    } else {
+      dropIcon(url, event.absoluteX, event.absoluteY);
+    }
+    supress = true;
+    setTimeout(() => { supress = false; }, 100);
   }
 })
+
+const dropIcon = (url: string, x?: number, y?: number) => {
+  let size = 64;
+  size = Math.round(size / figma.viewport.zoom);
+  
+  const node = figma.createRectangle() 
+  node.resize(size, size)
+
+  let viewportCenter = figma.viewport.center;
+  node.x = Math.round((x || viewportCenter.x) - size / 2);
+  node.y = Math.round((y || viewportCenter.y) - size / 2);
+
+  node.fills = [];
+  figma.createImageAsync(url).then(async (image: Image) => {
+    figma.currentPage.selection = [node];
+    node.fills = [{
+      type: 'IMAGE',
+      imageHash: image.hash,
+      scaleMode: 'FILL'
+    }]
+  })
+}
